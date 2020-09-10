70693ae2b2cda688a2b16951a18860c3cff042a1
Marcin Kwiatkowski <contact@frodigo.com>
2020-09-10 09:35:52 +0200

    Global variables and composing

--- src/components/SampleComponent/sample-component.js
+++ src/components/SampleComponent/sample-component.js
@@ -1,14 +1,18 @@
 import React from "react";
 import classes from './sample-component.css';
+import './global-styles.css'
 
 const SampleComponent = () => {
     return (
         <div className={classes.section}>
+            <header className={classes.sampleHeader}>Everything what you need to know about CSS modules</header>
             <h3 className={classes.heading}>This is the sample component tilte</h3>
             <p>This is the sample component paragraph</p>
+            <p className="global-class-name">This is paragraph with global styles appiled</p>
 
-            <h4 className={classes.heading}>This is the sample list:</h4>
-            <ul>
+            <h4 className={classes.secondaryHeading}>This is the sample list:</h4>
+            <h5 className={classes.redHeading}>Sample red heading</h5>
+            <ul className={classes['my-sample-class']}>
                 <li>Sample item A</li>
                 <li>Sample item B</li>
                 <li>Sample item C</li>
